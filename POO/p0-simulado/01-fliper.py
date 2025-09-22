eeb7a0af2ffe4c293668933909782413b37c5aa3
Lucas Keller <lucaskeller27@gmail.com>
2025-09-22 20:37:00 -0300

    Começar p0-03

--- POO/p0-simulado/01-fliper.py
+++ POO/p0-simulado/01-fliper.py
@@ -1,7 +1,9 @@
+# Leitura dos valores de P e R
 p, r = [int(w) for w in input().split()]
 
 buraco = ""
 
+# Definição das posições de P e R
 if (0 <= p <= 1) and (0 <= r <= 1):
     if (p == 0):
         buraco = "C"
---
+++ POO/p0-simulado/02-distancia.py
@@ -0,0 +1,8 @@
+# Leitura do valor da distância
+distancia = int(input())
+
+# Cálculo do tempo
+tempo = distancia * 2
+
+# Entrega do valor do tempo
+print(f"{tempo} minutos")
