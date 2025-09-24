ec571f4673afde99aeab36bfeafc8503e92aaa8e
Lucas Keller <lucaskeller27@gmail.com>
2025-09-24 20:00:22 -0300

    Melhorar p0-03

--- POO/p0-simulado/03-detector-de-colisoes.py
+++ POO/p0-simulado/03-detector-de-colisoes.py
@@ -1,12 +1,6 @@
 # Leitura dos valores dos dois retângulos
-x1a, y1a, x2a, y2a = [int(w) for w in input().split()] # A
-x1b, y1b, x2b, y2b = [int(w) for w in input().split()] # B
-
-# Colocar os valores em ordem crescente
-ax1, ax2 = min(x1a, x2a), max(x1a, x2a) # X do A
-ay1, ay2 = min(y1a, y2a), max(y1a, y2a) # Y do A
-bx1, bx2 = min(x1b, x2b), max(x1b, x2b) # X do B
-by1, by2 = min(y1b, y2b), max(y1b, y2b) # Y do B
+ax1, ay1, ax2, ay2 = [int(w) for w in input().split()] # A
+bx1, by1, bx2, by2 = [int(w) for w in input().split()] # B
 
 # Verificar se NÃO HÁ interseção
 if (ax2 < bx1) or (bx2 < ax1) or (ay2 < by1) or (by2 < ay1):
